31930ef26cb30bdc57da51fc846236b12753c995
Bruno SANCHEZ <bruno.sanchez@duke.edu>
2021-08-24 15:02:34 -0400

    Fixed annoying double linebreak

--- mkauthlist/mkauthlist.py
+++ mkauthlist/mkauthlist.py
@@ -513,7 +513,7 @@ if __name__ == "__main__":
             document = aanda_document
             authlist = aanda_authlist
             affilmark = r'\inst{%s},'
-            affiltext = r'\and %s \n'
+            affiltext = r'\and %s '
         else:
             msg = "Unrecognized latex class: %s"%cls
             raise Exception(msg)
